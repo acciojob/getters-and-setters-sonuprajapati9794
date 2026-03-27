06f83480bd1141796e219a4d5a3f8bf0420ab5be
sonuprajapati9794 <prajapati979473@gmail.com>
2026-03-27 23:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ class Person {
     this._age = age;
   }
 
-  // Getter for name
+
   get name() {
     return this._name;
   }
 
-  // Getter for age
+
   get age() {
     return this._age;
   }
 
-  // Setter for age
+
   set age(newAge) {
     this._age = newAge;
   }
@@ -33,15 +33,6 @@ class Teacher extends Person {
 }
 
 
-// Testing
-const person = new Person("John", 25);
-console.log(person.name); // John
-
-person.age = 30;
-console.log(person.age); // 30
-
-const student = new Student("Alice", 22);
-student.study(); // Alice is studying
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach(); // Bob is teaching
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
